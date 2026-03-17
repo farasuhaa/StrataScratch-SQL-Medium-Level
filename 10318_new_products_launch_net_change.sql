e1763cf62416f27ba1654f741d953e0f3d9fdf77
Farah Mohd <122101307+farasuhaa@users.noreply.github.com>
2026-03-17 13:57:10 +0800

    Update 10318_new_products_launch_net_change.sql

--- 10318_new_products_launch_net_change.sql
+++ 10318_new_products_launch_net_change.sql
@@ -7,14 +7,14 @@
 
 -- Problem:
 -- Calculate the net change in the number of products launched by companies in 2020 compared to 2019. 
---
+
 -- Output: company_name | net_difference.
---
+
 -- Note:
 -- (Net difference = Number of products launched in 2020 - The number launched in 2019.)
 
 -- Tables:
---
+
 -- car_launches
 -- __________________________
 -- | company_name  | text    |
@@ -23,10 +23,9 @@
 
 -- =====================================================================================
 -- Approach
---
+
 -- 1. Find the total number of products launched for each year.
 -- 2. Calculate the net change of products launched.
---
 -- =====================================================================================
 
 with base as (
